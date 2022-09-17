89e5b5102255ddee3483f3d2b8ba94f9cba68c0a
Humdinger93 <94093748+Humdinger93@users.noreply.github.com>
2022-09-17 08:45:04 -0400

    raw data

--- main.js
+++ main.js
@@ -18,45 +18,7 @@ document.getElementById("csv").innerHTML = rawData;
 //developer info
 console.log(rawData);
 console.log("rawData type: " + typeof rawData);
-
-//Part 2: CSV to JSON
-//credit: stavck overflow user "Paath" on 2016-05-07
-
-//initilize variables
-let arrayOne = rawData.split("\r\n");
-let header = arrayOne[0].split(",");
-let nOfRow = arrayOne.length;
-let noOfCol = header.length;
-let jsonData = [];
-let i = 0;
-let j = 0;
-
-//for loop rows
-for (i=1;i < nOfRow - 1; i++) {
-    let obj = [];
-    let myNewLine = arrayOne[i].split(",");
-        //nested loop for columns
-        for (j = 0; j < noOfCol; j++) {
-            obj[header[j]] = myNewLine[j];
-        };
-        //generate json
-        jsonData.push(obj);
-};
-
-
 }
-//publish data
-document.getElementById("json").innerHTML =jsonData;
-
-//Developer info
-console.log(jsonData);
-console.table(jsonData);
-console.log("jsonData type: " +typeof jsonData);
-
-//stringify
-jsonString= JSON.stringify(jsonData);
-console.log(jsonString);
-console.log("jsonString type: " +typeof jsonString);
 
 
 
